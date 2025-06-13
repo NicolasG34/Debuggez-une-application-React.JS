b1106e4987a16e710f0cfd1784a59c8ccbe2726e
Nicolas Gottiniaux <nicolas.gottiniaux@gmail.com>
2025-06-13 13:17:05 +0200

    correction undefined map sur page principale

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,33 +13,32 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = data?.events.filter((event)=> event.type === type || !type  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+
+  const events = data?.events || [];
+
+  const filteredEvents = events
+    .filter((event) => !type || event.type === type)
+    .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
-
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+
+  const pageNumber = Math.ceil(events.filter((event) => !type || event.type === type).length / PER_PAGE);
+  const typeList = new Set(events.map((event) => event.type));
+
   return (
     <>
-      {error && <div>An error occured</div>}
-      {data === null ? (
+      {error && <div>An error occurred</div>}
+      {!data ? (
         "loading"
       ) : (
         <>
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => value ? changeType(value) : changeType(null)}
+            onChange={(value) => changeType(value || null)}
           />
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
@@ -47,7 +46,7 @@ const EventList = () => {
                 {({ setIsOpened }) => (
                   <EventCard
                     onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
+                    imageSrc={event.cover || "fallback.jpg"}
                     title={event.title}
                     date={new Date(event.date)}
                     label={event.type}
@@ -57,8 +56,7 @@ const EventList = () => {
             ))}
           </div>
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
+            {[...Array(pageNumber)].map((_, n) => (
               <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
